4f17e6f7c6aa14ec61212b606779fb7260cf32d9
VaibhavV3 <vaibhavwalekar2013@gmail.com>
2025-02-16 16:27:27 +0000

    Update OrderItemService.java build error

--- src/main/java/org/example/service/OrderItemService.java
+++ src/main/java/org/example/service/OrderItemService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.example.entity.OrderItem;
+
 @Service
 @Transactional
 public class OrderItemService {
