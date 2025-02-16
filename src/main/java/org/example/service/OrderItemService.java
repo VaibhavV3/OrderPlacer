4a8919cbf81567b94200f5147d8f6c06225e4e86
VaibhavV3 <vaibhavwalekar2013@gmail.com>
2025-02-16 16:25:20 +0000

    Update OrderItemService.java

--- src/main/java/org/example/service/OrderItemService.java
+++ src/main/java/org/example/service/OrderItemService.java
@@ -6,6 +6,10 @@ import java.util.Optional;
 import org.example.repository.OrderRepository;
 import org.example.repository.OrderItemRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @Service
 @Transactional
 public class OrderItemService {
